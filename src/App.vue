add3a5eb7207f72ef639c56819db7e4ac93a5e9e
sevro49 <21120205303@ismu.edu.tr>
2023-07-30 14:42:32 +0300

    starting a new game

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,38 @@
 <template>
-  <h1>Reaction Timer</h1>
+    <h1>Reaction Timer</h1>
+    <button @click="start" :disabled="isPlaying">Play!</button>
+    <Block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
-
+import Block from "./components/Block.vue";
 export default {
-  name: 'App',
-  components: {
-  }
-}
+    name: "App",
+    components: { Block },
+
+    data() {
+        return {
+            isPlaying: false,
+            delay: null,
+        };
+    },
+
+    methods: {
+        start() {
+            this.delay = 2000 + Math.random() * 5000;
+            this.isPlaying = true;
+        },
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #444;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #444;
+    margin-top: 60px;
 }
 </style>
