990ab1ce4e13039aac7eddca344183972704e34c
sevro49 <21120205303@ismu.edu.tr>
2023-07-30 22:56:49 +0300

    creating a timer

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="block" v-if="showBlock">click me</div>
+    <div class="block" v-if="showBlock" @click="stopTimer">click me</div>
 </template>
 
 <script>
@@ -9,22 +9,29 @@ export default {
     data() {
         return {
             showBlock: false,
+            timer: null,
+            reactionTime: 0,
         };
     },
 
     mounted() {
-        console.log("component mounted");
         setTimeout(() => {
             this.showBlock = true;
+            this.startTimer();
         }, this.delay);
     },
 
-    updated() {
-        console.log("component updated");
-    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10;
+            }, 10);
+        },
 
-    unmounted() {
-        console.log("unmounted");
+        stopTimer() {
+            clearInterval(this.timer);
+            console.log(this.reactionTime)
+        },
     },
 };
 </script>
